cf2ab03ed0915728c93b17692df85d8bd2a1ff6c
Ubuntu <ubuntu@ip-172-31-44-37.us-east-2.compute.internal>
2019-12-11 00:34:14 +0000

    Assessment 2

--- js-foundations.js
+++ js-foundations.js
@@ -1,103 +1,115 @@
-// ASSESSMENT 2: JAVASCRIPT FOUNDATIONS
-// Coding practical questions
+// // ASSESSMENT 2: JAVASCRIPT FOUNDATIONS
+// // Coding practical questions
 
-// 1. Write a function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
-const div3 = (...number) => {     
-    if(number % 3 === 0){
-    return number + " is divisible by 3"
-} else {
-    return number + " is not divisible by 3"
- }
+// // 1. Write a function that takes a number as an argument and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
+// const div3 = (...number) => {     
+//     if(number % 3 === 0){
+//     return number + " is divisible by 3"
+// } else {
+//     return number + " is not divisible by 3"
+//  }
+// }
+
+// console.log(div3(15))
+// console.log(div3(0))
+// console.log(div3(385))
+// console.log(div3(-7))
+
+
+// // 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
+// var helloMe = {
+//     firstName: "Michael", 
+//     lastName: "Abdelshahid",
+//     nationality: "Egyptian", 
+//     languages: " Arabic, English and American Sign Language"
+//     introMe: function () {
+//         return `Hello, My name is ${this.firstName} ${this.lastName}. I am ${this.nationality} and speak three languages:${this.languages}`
+//     }
+// }
+// console.log(helloMe.introMe())
+
+// // ------------------------------------- Consider the variable:
+
+// var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"];
+
+// // 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
+// const everyItem = (array) =>{
+//     let item = array.filter((value, index) =>{
+//         return index%2 === 0
+//     })
+//     return item.join(" ")
+// }
+// console.log(everyItem(randomNouns))
+
+// // 3b. Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+// const cap = (array) =>{
+//     return array.map(value =>{
+//         return value[0].charAt(0).toUpperCase().concat(value.slice(1))
+//     })
+// }
+// console.log(cap(randomNouns))
+
+
+
+// // 3c. STRETCH: Create a function that returns the length of each string from the randomNouns variable. Expected output: [10, 6, 5, 10, 6, 11, 8]
+// const stringLen = (array) =>{
+//     return array.map(value =>{
+//         return value.length
+//     })
+// }
+// console.log(stringLen(randomNouns))
+
+
+
+// // -------------------------------------- Consider the variables:
+// var testString1 = "learn"
+// var testString2 = "academy"
+// var testString3 = "sandiego"
+
+// // 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
+// const aBC = (string) => {
+//     return string.split("").sort().join("")
+// }
+// console.log(aBC(testString1))
+// console.log(aBC(testString2))
+// console.log(aBC(testString3))
+
+// // 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
+// const aBCOrder = (...strings) => {
+//     let aBCStrings = [...strings]
+//     return aBCStrings.join("").split("").sort().join("")
+// }
+// console.log(aBCOrder(testString1, testString2, testString3))
+
+// // ------------------------------------- Consider the variables:
+// var amounts = [9, 1, 8, 16, 5, 1, 42]
+// var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguins", "llamas"]
+
+// // 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
+// const combinedStrings = (array1, array2) => {
+//     let combined = array1.map((value, index) => {
+//         return value + " " + array2[index]
+//     })
+//     return combined.join(" ")
+// }
+// console.log(combinedStrings(amounts, animals))
+
+// // 5b. STRETCH: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
+// const combinedStrings2 = (array1, array2) => {
+//     let combined = array1.map((value, index) => {
+//         if(value === 1){
+//             return `1 ${array2[index].slice(0, array2[index].length-1)}`
+//         } else {
+//             return value + " " + array2[index]
+//         }
+//     })
+//     return combined.join(" ")
+// }
+// console.log(combinedStrings2(amounts, animals))
+
+var text = 'outside'
+function logIt(){
+  console.log(text)
+  var text = 'inside'
 }
-
-console.log(div3(15))
-console.log(div3(0))
-console.log(div3(385))
-console.log(div3(-7))
-
-
-// 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence using the object properties to describe you.
-var helloMe = [(firstName="Michael"), (lastName = "Abdelshahid"), (nationality = "Egyptian"), (languages = " Arabic, English and American Sign Language")]
-    const myInfo = (array)=>{
-        return ['Hello, My name is ${this.firstName} ${this.lastName}. I am ${this.nationality} and speak three languages:${this.languages}']
-    }
-console.log(helloMe(myInfo)
-
-// ------------------------------------- Consider the variable:
-
-var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"];
-
-// 3a. Write a function that returns every other item from the array as one string. Expected output: "streetlamp teeth nephew database"
-const everyItem = (array) =>{
-    let item = array.filter((value, index) =>{
-        return index%2 === 0
-    })
-    return item.join(" ")
-}
-console.log(everyItem(randomNouns))
-
-// 3b. Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
-const cap = (array) =>{
-    return array.map(value =>{
-        return value[0].charAt(0).toUpperCase().concat(value.slice(1))
-    })
-}
-console.log(cap(randomNouns))
-
-
-
-// 3c. STRETCH: Create a function that returns the length of each string from the randomNouns variable. Expected output: [10, 6, 5, 10, 6, 11, 8]
-const stringLen = (array) =>{
-    return array.map(value =>{
-        return value.length
-    })
-}
-console.log(stringLen(randomNouns))
-
-
-
-// -------------------------------------- Consider the variables:
-var testString1 = "learn"
-var testString2 = "academy"
-var testString3 = "sandiego"
-
-// 4a. Write a function called alphabetSoup that takes one argument and returns a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
-const aBC = (string) => {
-    return string.split("").sort().join("")
-}
-console.log(aBC(testString1))
-console.log(aBC(testString2))
-console.log(aBC(testString3))
-
-// 4b. Write a function that takes in all three variables and returns a string with all the letters in alphabetical order. Expected output: "aaaacddeeegilmnnorsy"
-const aBCOrder = (...strings) => {
-    let aBCStrings = [...strings]
-    return aBCStrings.join("").split("").sort().join("")
-}
-console.log(aBCOrder(testString1, testString2, testString3))
-
-// ------------------------------------- Consider the variables:
-var amounts = [9, 1, 8, 16, 5, 1, 42]
-var animals = ["ducks", "elephants", "pangolins", "zebras", "giraffes", "penguins", "llamas"]
-
-// 5a. Write a function that takes the two variables and returns a combined string. Expected output: "9 ducks 1 elephants 8 pangolins 16 zebras 5 giraffes 1 penguins 42 llamas"
-const combinedStrings = (array1, array2) => {
-    let combined = array1.map((value, index) => {
-        return value + " " + array2[index]
-    })
-    return combined.join(" ")
-}
-console.log(combinedStrings(amounts, animals))
-
-// 5b. STRETCH: Create a function that combines the two variables and updates the animal to be singular if the animal's corresponding number is 1. Expected output: "9 ducks 1 elephant 8 pangolins 16 zebras 5 giraffes 1 penguin 42 llamas"
-const combinedStrings2 = (array1, array2) => {
-    let combined = array1.map((value, index) => {
-        if(value === 1){
-            return `1 ${array2[index].slice(0, array2[index].length-1)}`
-        } else {
-            return value + " " + array2[index]
-        }
-    })
-    return combined.join(" ")
-}
-console.log(combinedStrings2(amounts, animals))
+logIt()
--- js-foundations.md
+++ js-foundations.md
@@ -16,65 +16,96 @@ logIt()
 1a. Look at this Javascript function and try to predict what the console show.
 The console shows the text 'outside'
 
-
 1b. Test the function. Explain why the function returned what it did.
 
   Your answer:
+  Undefined because it's not the proper print function for javacript. 
 
+  
   Researched answer:
-
+  Undefined because the correct syntax is console.log.
 
 2. What is JSON?
 
   Your answer:
+  Javascript Object Nesting.
 
   Researched answer:
+  JSON (Javascript Object Notation) is a lightweight data-interchange format
 
 
 3. What does CRUD stand for?
 
   Your answer:
-
+  Create
+  Read
+  Update
+  Delete
+  
   Researched answer:
-
-
+  Create
+  Read
+  Update
+  Delete
 
 4. What does are the 5 HTTP verbs?
 
   Your answer:
+  GET
+  POST
+  DELETE
+  PRINT
+  UPDATE
 
   Researched answer:
+  GET
+  POST 
+  DELETE
+  PATCH
+  PUT 
 
 
 5. What is a higher-order function?
 
   Your answer:
+  High-order function that carry more than one function in an argument along with a return statment. 
 
   Researched answer:
-
+  Higher-order functions are functions that take other functions as arguments or 
+  return functions as their results.
 
 6. STRETCH: What is a closure, what is it good for and how do you recognize one?
 
   Your answer:
-
+  Closure is a function in (). It is good for reusing over and over 
+  to put in proper variable. 
+  
   Researched answer:
-
+  A closure is the combination of a function bundled together (enclosed) with 
+  references to its surrounding state (the lexical environment). In other words, 
+  a closure gives you access to an outer function's scope from an inner function.
+  displayName() is the inner function, a closure. 
 
 7. STRETCH: What is an API?
 
   Your answer:
+  API is Application Programming Information. 
 
   Researched answer:
-
+  API stands for Application Program Interface, which can be defined as a set of 
+  methods of communication between various software components. In other words, 
+  an API allows software to communicate with another software.
 
 ### Additional Feedback Questions.
 
 1. Do you have a suggestion for a Check In question?
-
+What type of clothes do you enjoy wearing daily?
 
 
 2. What was your favorite topic this week?
-
+React and creating a react app.
 
 
 3. What was your "A-ha!" moment this week?
+Using the if/else statement inside of another if/else statement. I never knew it
+is possible and thought you can only use an argument by itself.
